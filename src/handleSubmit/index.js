69d3f91b67fe23af48f91c6b4fba9eb10fd83448
Anna Spysz <anna.spysz@stackery.io>
2019-11-07 12:35:17 -0800

    First version of handler function

--- src/handleSubmit/index.js
+++ src/handleSubmit/index.js
@@ -1,6 +1,35 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
-  return {};
+exports.handler = async event => {
+  const content = JSON.stringify(event.body);
+  try {
+    await uploadFormContent(content);
+
+    console.log('Succeeded in uploading form content!')
+  } catch (err) {
+    console.error('Failed to upload form content:');
+    console.error(err);
+
+    // Re-throw error to ensure invocation is marked as a failure
+    throw err;
+  }
 };
+
+// Upload form content from event
+async function uploadFormContent(content) {
+
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: content,
+    ContentType: 'application/json',
+    ACL: 'public-read'
+  }
+
+  try {
+    await s3.putObject(params).promise();
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
